cc7c25f29bc3d9a40a144f9af81a699329ca994d
Aleksandar <al.stojanovic0@gmail.com>
2024-01-24 15:53:47 +0100

    Added mean square and euclidean edge comparisons
    Not so beneficial

--- puzzle/src/comparing.rs
+++ puzzle/src/comparing.rs
@@ -1,7 +1,7 @@
-use image::{DynamicImage, GenericImageView};
+use image::{DynamicImage, GenericImageView, Rgb, Rgba};
 
 // Compares right edge of img1 and left edge of img2 and returns difference
-pub fn compare_right_edge(img1: &DynamicImage, img2: &DynamicImage) -> u32{
+pub fn compare_right_edge_abs(img1: &DynamicImage, img2: &DynamicImage) -> u32{
     let mut difference = 0;
 
     if img1.height() != img2.height(){
@@ -24,4 +24,64 @@ pub fn compare_right_edge(img1: &DynamicImage, img2: &DynamicImage) -> u32{
 
 
     return difference as u32
+}
+
+
+//JOS JE GORE AAAAAAAAAAAAAAAA
+pub fn compare_right_edge_sq(img1: &DynamicImage, img2: &DynamicImage) -> u32{
+    let mut difference = 0;
+
+    if img1.height() != img2.height(){
+        return u32::MAX;
+    }
+
+    for height_idx in 0..img1.height(){
+        let mut pixel_diff = 0;
+        // Right edge
+        let pixel1 = img1.get_pixel(img1.width() - 1, height_idx);
+        // Left edge
+        let pixel2 = img2.get_pixel(0, height_idx);
+
+        for channel_idx in 0..4{
+            let  channel_diff = pixel1[channel_idx] as i32 - pixel2[channel_idx] as i32;
+            pixel_diff += channel_diff.pow(2);
+        }
+        difference += pixel_diff;
+    }
+
+
+    return difference as u32
+}
+
+//MALO BOLJEEEEEEE
+pub fn compare_right_edge_euclidean(img1: &DynamicImage, img2: &DynamicImage) -> u32{
+    let mut difference = 0;
+
+    if img1.height() != img2.height(){
+        return u32::MAX;
+    }
+
+    for height_idx in 0..img1.height(){
+        // Right edge
+        let pixel1 = img1.get_pixel(img1.width() - 1, height_idx);
+        // Left edge
+        let pixel2 = img2.get_pixel(0, height_idx);
+
+        let pixel_diff = euclidean_distance_rgba(pixel1, pixel2) as u32;
+
+        difference += pixel_diff;
+    }
+
+
+    return difference as u32
+}
+
+
+fn euclidean_distance_rgba(p1: Rgba<u8>, p2: Rgba<u8>) -> f64 {
+    let r_diff = f64::from(p2[0] as i32 - p1[0] as i32);
+    let g_diff = f64::from(p2[1] as i32 - p1[1] as i32);
+    let b_diff = f64::from(p2[2] as i32 - p1[2] as i32);
+    let a_diff = f64::from(p2[3] as i32 - p1[3] as i32);
+
+    (r_diff.powi(2) + g_diff.powi(2) + b_diff.powi(2) + a_diff.powi(2)).sqrt()
 }
--- puzzle/src/main.rs
+++ puzzle/src/main.rs
@@ -8,14 +8,35 @@ use image::{DynamicImage, GenericImage, GenericImageView, ImageResult};
 
 
 fn main() {
-    let solved_path = "solved/test.png";
+    let solved_path = "solved/slika1.jpg";
     let solved_image = image::open(solved_path).unwrap_or_else(|_| {
         eprintln!("Failed to open image: {:?}", solved_path);
         DynamicImage::new_rgba8(1, 1)
     });
 
-    let mut pieces = piece::load_images_from_folder("test");
+
+    let mut pieces = piece::load_images_from_folder("slika1");
     let mut pieces_ro = pieces.clone();
+
+    //TESTING
+
+    let piece3 = &pieces[5];
+    let piece1 = &pieces[1];
+    let piece9 = &pieces[11];
+    let edgy_image = DynamicImage::new_rgba8(1,10);
+
+    let difference31 = comparing::compare_right_edge_euclidean(&piece3.image, &piece1.image);
+    let difference19 = comparing::compare_right_edge_euclidean(&piece1.image, &piece9.image);
+    let difference_same_edge = comparing::compare_right_edge_euclidean(&edgy_image, &edgy_image);
+
+    println!("DIFF 3 and 1: {}", difference31);
+    println!("DIFF 1 and 9: {}", difference19);
+    println!("DIFF same edge: {}", difference_same_edge);
+
+    let edgy_image = DynamicImage::new_rgba8(1,10);
+
+   return;
+    //TESTING
 
     let (common_width, common_height) = piece::find_most_common_dimensions(&pieces);
     let horizontal_pieces_num = solved_image.width() / common_width;
@@ -47,7 +68,7 @@ fn main() {
                     continue;
                 }
 
-                let difference = comparing::compare_right_edge(&piece.image, &comparing_piece.image);
+                let difference = comparing::compare_right_edge_euclidean(&piece.image, &comparing_piece.image);
                 if difference < min_diff {
                     min_index = comparing_piece.index;
                     min_diff = difference;
@@ -81,7 +102,7 @@ fn main() {
                     let disputed_piece = &pieces_ro[contestant.0 as usize];
                     let contestant_piece = &pieces_ro[contestant.1 as usize];
 
-                    let difference = comparing::compare_right_edge(&contestant_piece.image, &disputed_piece.image);
+                    let difference = comparing::compare_right_edge_euclidean(&contestant_piece.image, &disputed_piece.image);
 
                     if difference < min_diff {
                         min_diff = difference;
@@ -131,7 +152,7 @@ fn main() {
                 solved_edge_idx += 1;
             }
 
-            let mut difference = comparing::compare_right_edge(&piece.image, &solved_edge);
+            let mut difference = comparing::compare_right_edge_euclidean(&piece.image, &solved_edge);
 
             //Matching
             //TODO: SCALE THRESHOLD
@@ -195,103 +216,3 @@ fn main() {
     }
 
 }
-
-
-// REDOSLED: C A Y Z -> 1 0 2 3
-
-
-// ***** LAST ROW MANUAL *****
-// let piece1 = image::open("jedan_red/part_c.jpg").expect("Failed to open");
-// let piece2 = image::open("jedan_red/part_a.jpg").expect("Failed to open");
-// let piece3 = image::open("jedan_red/part_y.jpg").expect("Failed to open");
-// let piece4 = image::open("jedan_red/part_z.jpg").expect("Failed to open");
-//
-// let mut final_result = stitching::stitch_right(&piece1, &piece2, 0);
-//  final_result = stitching::stitch_right(&final_result, &piece3, 0);
-//  final_result = stitching::stitch_right(&final_result, &piece4, 0);
-
-// final_result.save("final.jpg").expect("FAILED");
-
-// **** LAST ROW RIGHT EDGE COMPARISON
-// let diff12 = comparing::compare_right_edge(&piece1, &piece2);
-// let diff13 = comparing::compare_right_edge(&piece1, &piece3);
-// let diff14 = comparing::compare_right_edge(&piece1, &piece4);
-// println!("Diff 1 and 2: {}", diff12);
-// println!("Diff 1 and 3: {}", diff13);
-// println!("Diff 1 and 3: {}", diff14);
-// println!();
-//
-//
-// let diff21 = comparing::compare_right_edge(&piece2, &piece1);
-// let diff23 = comparing::compare_right_edge(&piece2, &piece3);
-// let diff24 = comparing::compare_right_edge(&piece2, &piece4);
-// println!("Diff 2 and 1: {}", diff21);
-// println!("Diff 2 and 3: {}", diff23);
-// println!("Diff 2 and 4: {}", diff24);
-// println!();
-//
-//
-// let diff31 = comparing::compare_right_edge(&piece3, &piece1);
-// let diff32 = comparing::compare_right_edge(&piece3, &piece2);
-// let diff34 = comparing::compare_right_edge(&piece3, &piece4);
-// println!("Diff 3 and 1: {}", diff31);
-// println!("Diff 3 and 2: {}", diff32);
-// println!("Diff 3 and 4: {}", diff34);
-// println!();
-
-
-
-//*****RESOLVE CONFLICTS WITH PIECES WITH SAME NEIGHBOR******
-//This also decides what is an edge piece
-//TODO: READONLY VECTOR NEEDS TO BE UPDATED
-
-// for piece_idx in 0..horizontal_pieces_num{
-//  .0 is idx of disputed piece, .1 is idx of piece that contains him as right neighbor
-// let mut contain_as_neighbor : Vec<(u32,u32)> = vec![];
-//
-// for piece in &pieces_ro{
-// if let Some(right_neighbor) = piece.right_neighbor {
-// if right_neighbor == piece_idx {
-// contain_as_neighbor.push((piece_idx, piece.index));
-// }
-// }
-// }
-//
-// if(contain_as_neighbor.len() > 1){
-// let mut min_diff = u32::MAX;
-// //TODO check safer solution
-// let mut min_idx = 0;
-//
-// for contestant in &contain_as_neighbor{
-// let disputed_piece = &pieces_ro[contestant.0 as usize];
-// let contestant_piece = &pieces_ro[contestant.1 as usize];
-//
-// let difference = comparing::compare_right_edge(&contestant_piece.image, &disputed_piece.image);
-//
-// if difference < min_diff{
-// min_diff = difference;
-// min_idx = contestant.1;
-// }
-// }
-//
-// //Contestant with min diff gets disputed piece as right neighbors and others get None
-// let mut winner = &mut pieces[min_idx as usize];
-// winner.right_neighbor = Some(piece_idx);
-// pieces_ro = pieces.clone();
-//
-// for contestant in &contain_as_neighbor{
-// if contestant.1 == min_idx{
-// continue;
-// }
-// let mut loser = &mut pieces[contestant.1 as usize];
-// loser.right_neighbor = None;
-// pieces_ro = pieces.clone();
-// }
-// }
-//
-// }
-//
-// println!("AFTER CONFLICT RESOLVING");
-// for piece in &pieces{
-// println!("{}", piece);
-// }
